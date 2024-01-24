db98e5eb9016b9e53922efd4b28c86c0a5f43620
Sara Ludwick <saraludwick@campus-061-255.ucdavis.edu>
2024-01-24 13:23:44 -0800

    read pdfs and begin tokenize code

--- scripts/cwpp_pdfs/pdf_processing.R
+++ scripts/cwpp_pdfs/pdf_processing.R
@@ -1,23 +1,19 @@
 library(pdftools)
 library(stringr)
 library(quanteda)
+library(tokenizers)
 
 #vector of pdf names
 pdfs <- list.files("large_data/cwpp_pdfs_CA", pattern = "pdf$", full.names = T)
 #apply pdf_text to each
 plans <- lapply(files, pdf_text)
-#did not like, errors but seemed to read it ok
+#did not like, errors but seemed to read it ok?
+#get filename without extension and file path
+names(plans) <- tools::file_path_sans_ext(basename(pdfs))
+#alternatively using regex: extract everything after the last / and before the last . 
+# names(plans) <- sub('.*/(.*)\\..*', '\\1', pdfs)
 
-#alternatively using tm to make a corpus
-library(tm)
-corp <- Corpus(URISource(pdfs),
-               readerControl = list(reader = readPDF))
-plans.tdm <- TermDocumentMatrix(corp, 
-                                   control = 
-                                     list(removePunctuation = TRUE,
-                                          stopwords = TRUE,
-                                          tolower = TRUE,
-                                          stemming = TRUE,
-                                          removeNumbers = TRUE,
-                                          bounds = list(global = c(3, Inf)))) 
-findFreqTerms(plans.tdm, lowfreq = 100, highfreq = Inf)
+#tokenize sentences, needs cleaning
+tokens <- lapply(plans, tokenize_sentences)
+
+
