d9f59df70deb7a4e4e269d71195b77aa5aff3ce9
Sara Ludwick <saraludwick@campus-060-082.ucdavis.edu>
2024-03-13 21:16:06 -0700

    added more compound tokens and removed community names from dfm

--- code/0X_simple_seeded_lda.R
+++ code/0X_simple_seeded_lda.R
@@ -25,43 +25,49 @@ toks <- tokens(recorp, remove_punct = TRUE, remove_symbols = TRUE, remove_number
 
 
 #### NOTE HERE WE PROBS WANT TO COMPOUNDS SOME IMPORTANT NGRAMS (E.G., DEFENSIBLE SPACE)
-comp_words <- c('defensible space', 'fuel break', 'building standards', 'residential development')
+comp_words <- c('defensible space*', 'fuel break*', 'building standard*', 'residential development*', 'emergency response*', 'emergency service*', 'cal fire', 'fire safe council*', 'water supply')
 toks <- quanteda::tokens_compound(toks, pattern = phrase(comp_words), case_insensitive = T)
 # kw_comp <- kwic(toks_comp, pattern = phrase(c(str_replace(comp_words, " ", "_"))))
 # head(kw_comp, 25)
 
+### want to remove place names, get map dictionary? starting with removing community name
+names <- str_extract(tx_files, pattern = "(?<=_)[^_]+(?=_)") %>%
+  str_split(pattern = "(?<=[a-z])(?=[A-Z])") %>%
+  unlist() %>%
+  unique() %>%
+  tolower()
+# this also removes names that might be important words like "lake", "trail", "salmon", "creek" etc. so figure that out, but for now manually remove obvious ones
+names <- names[!names %in% c("lake", "trail", "salmon", "creek", "slope")]
+
 
 dfmt <- dfm(toks) %>%
-  dfm_remove(stopwords("en"), min_nchar = 2) %>%
+  dfm_remove(c(stopwords("en"), names), min_nchar = 2) %>%
   ## needs to be in at 5 docs
   dfm_trim(min_docfreq = 5, docfreq_type = "count")
-### some of these will change/improve with ngrams
-dict <- dictionary(list(defensible_space = c("defensible"),
-                        fuelbreak = c("fuelbreak"),
-                        thinning = c("thinning",'brushing'),
-                        prescribed = c("prescribed","intentional"),
-                        recruit = c("retain","recruit","volunteer"),
-                        educate = c("educat*","outreach"),
-                        code = c('code','building'),
-                        #### let's think of some topics we want to id but group our
-                        evacuation = c("evacuation","emergency")))
-#### we need to play with the residual and use that to gauge model GOF
-### e.g., how does altering the number of "other" topics shape semantic coherence, modularity, etc.
-lda_seed <- textmodel_seededlda(dfmt, dict, min_termfreq = 10,
-                                max_iter = 500,residual = 10)
 
-mgmt_strat_dict <- dictionary(list(defensible_space = c("defensible"),
-                                   fuelbreak = c("fuelbreak"),
+
+#temporary dictionaries, need to discuss how many terms per topic and refine topics
+mgmt_dict <- dictionary(list(defensible_space = c("defensible_space"),
+                                   fuelbreak = c("fuelbreak", "fuel_break"),
                                    thinning = c("thinning",'brushing'),
                                    prescribed = c("prescribed","intentional", "beneficial"),
-                                   emergency = c("emergency", "response"),
+                                   emergency = c("emergency", "emergency_response", "emergency_service*"),
                                    landscaping = c('landscaping'),
-                                   water = c("water supply"),
-                                   code = c('code','building')))
-                                   
-implmnt_strat_dict <- dictionary(list(recruit = c("retain","recruit","volunteer"),
-                                educate = c("educat*","outreach"),
-                                prepare = c("evacuat*", "prepar*"),
-                                firewise = c("firewise"),
-                                comms = c("communicat*")
-                                ))
+                                   water = c("water", "water_supply"),
+                                   code = c('code','building_standard*'),
+                                   develop = c("residential*")))
+
+implmnt_dict <- dictionary(list(recruit = c("retain","recruit","volunteer"),
+                                      educate = c("educat*","outreach"),
+                                      evac = c("evacuat*"),
+                                      firewise = c("firewise"),
+                                      comms = c("communicat*")))
+
+#### we need to play with the residual and use that to gauge model GOF
+### e.g., how does altering the number of "other" topics shape semantic coherence, modularity, etc.
+
+lda_seed <- textmodel_seededlda(dfmt, c(mgmt_dict, implmnt_dict), min_termfreq = 10,
+                                max_iter = 500,residual = 10)
+
+terms(lda_seed, n = 10)
+topics <- topics(lda_seed)
